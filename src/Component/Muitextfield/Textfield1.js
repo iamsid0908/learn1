2b414e121cc803c7459ef8584685144cac62736a
Siddharth <siddharth@localhost.localdomain>
2023-03-12 18:52:17 +0530

    for cicd

---
+++ src/Component/Muitextfield/Textfield1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Stack } from '@mui/system'
+import TextField from '@mui/material/TextField';
+
+function Textfield1() {
+  return (
+    <Stack spacing={4}>
+        <Stack direction='row' spacing={4}>
+            <TextField label="name" variant='outlined'/>
+            <TextField label="name" variant='filled'/>
+            <TextField label="name" variant='standard'/>
+        </Stack>
+        <Stack direction='row' spacing={4}>
+            <TextField label="name" variant='outlined' required/>
+            <TextField label="name" variant='outlined' helperText='do not share yr password'/>
+            <TextField label="password" variant='outlined' type="password"/>
+            <TextField label="name" variant='outlined' disabled/>
+            <TextField label="read-only" variant='outlined' inputProps={{readOnly:"hi"}}/>
+        </Stack>
+    </Stack>
+  )
+}
+
+export default Textfield1
